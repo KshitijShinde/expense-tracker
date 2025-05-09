6cdb49b3a052ee03fc2cdbb89af4ce56b4012eac
Kshitij Shinde <kshitijshinde3090@gmail.com>
2025-05-09 16:02:34 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,24 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import streamlit as st
-import json
 
-if not firebase_admin._apps:
-    firebase_dict = json.loads(st.secrets["FIREBASE_CREDENTIALS"])
-    cred = credentials.Certificate(firebase_dict)
+@st.cache_resource
+def get_firestore_client():
+    # Load the credentials from Streamlit secrets
+    cred = credentials.Certificate({
+        "type": st.secrets["type"],
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["private_key_id"],
+        "private_key": st.secrets["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+        "universe_domain": st.secrets["universe_domain"]
+    })
     firebase_admin.initialize_app(cred)
+    return firestore.client()
 
-db = firestore.client()
+db = get_firestore_client()

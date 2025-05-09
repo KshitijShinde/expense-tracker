dd6d9f52f182c302a5a1d821438f35d24728b327
Kshitij Shinde <kshitijshinde3090@gmail.com>
2025-05-09 15:27:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,16 @@ import seaborn as sns
 
 import os
 
-# Get environment variables for database connection
+
+
+# Retrieve environment variables
 DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
+DB_PORT = int(os.getenv('DB_PORT'))  # Convert to integer
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 
-# Test connection to PostgreSQL
+# Attempt to connect to the PostgreSQL database
 try:
     conn = psycopg2.connect(
         host=DB_HOST,
@@ -24,10 +26,10 @@ try:
         user=DB_USER,
         password=DB_PASSWORD
     )
-    st.success("Successfully connected to the database!")
+    print("Connected successfully")
 except psycopg2.OperationalError as e:
-    st.error(f"Error connecting to the database: {e}")
-)
+    print(f"Error: {e}")
+
 
 # Initialize cursor
 cursor = conn.cursor()
